07cc433584be376b4a5973eaa81b408cc373db44
Emma <emma.yeroshek@gmail.com>
2021-09-08 11:57:35 +0300

    Create Valiate form

--- app/forms/article/create_form.rb
+++ app/forms/article/create_form.rb
@@ -1,14 +1,7 @@
 # frozen_string_literal: true
 
 # module Article
-  class Article::CreateForm
-    include ActiveModel::Model
-
-    attr_accessor :content, :category, :article_tags, :article, :user
-
-    validates :category, :content, :article_tags, presence: true
-    validate :user_is_admin?, :tag_ids_exists?
-
+  class Article::CreateForm < Article::ValidateForm
     def initialize(params, user)
       @user = user
 
@@ -28,14 +21,6 @@
     end
 
     private
-
-    def user_is_admin?
-      errors.add(:base, 'You are not authorised for this action') unless user.is_a?(Admin)
-    end
-
-    def tag_ids_exists?
-      errors.add(:base, 'Tag ids invalid') unless article_tags[:tag_ids].all? { |tag_id| Tag.find_by(id: tag_id) }
-    end
 
     def create_article_tags!
       article_tags[:tag_ids].each { |tag_id| article.article_tags.create!(tag_id: tag_id) }
