5723ee1657d6275af25ec7f1ca8d08d546a061b3
Emma <emma.yeroshek@gmail.com>
2021-09-21 11:56:26 +0300

    Update deploy.rb

--- config/deploy.rb
+++ config/deploy.rb
@@ -57,6 +57,15 @@ namespace :puma do
 end
 
 namespace :deploy do
+  desc 'Upload config files'
+  task upload_configs: ['deploy:check:linked_dirs'] do
+    on roles(:all) do
+      execute "mkdir -p #{shared_path}/config"
+      upload! 'config/database.production.yml', "#{deploy_to}/shared/config/database.yml"
+      upload! 'config/secrets.production.yml', "#{deploy_to}/shared/config/secrets.yml"
+    end
+  end
+
   desc "Make sure local git is in sync with remote."
   task :check_revision do
     on roles(:app) do
