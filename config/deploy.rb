13cf1aa4f8a07cade93f144e9c07124e6bb69d28
Emma <emma.yeroshek@gmail.com>
2021-09-21 11:30:22 +0300

    Set up Puma, Nginx & Capistrano

---
+++ config/deploy.rb
@@ -0,0 +1,91 @@
+# config valid for current version and patch releases of Capistrano
+lock '~> 3.16.0'
+
+server 'ec2-18-220-228-28.us-east-2.compute.amazonaws.com', post: '18.220.228.28', user: 'ubuntu', roles: %w{app db web}, primary: true
+
+set :application, 'articles_catalog'
+set :repo_url, 'git@github.com:mmrshk/articles_catalog.git'
+set :user, 'ubuntu'
+ask :branch, :main
+
+append :linked_files, *%w(
+  config/database.yml
+  config/secrets.yml
+)
+
+append :linked_dirs, *%w(
+  log
+  public/system
+  public/uploads
+  tmp/cache
+  tmp/pids
+  tmp/sockets
+  vendor/bundle
+)
+
+set :keep_releases, 5
+
+set :rvm_type, :user
+set :rvm_ruby_version, 'ruby-2.7.1'
+
+# Puma config
+set :pty,             true
+set :use_sudo,        false
+set :stage,           :production
+set :deploy_via,      :remote_cache
+set :deploy_to,       "/home/#{fetch(:user)}/apps/#{fetch(:application)}"
+set :puma_bind,       "unix://#{shared_path}/tmp/sockets/#{fetch(:application)}-puma.sock"
+set :puma_state,      "#{shared_path}/tmp/pids/puma.state"
+set :puma_pid,        "#{shared_path}/tmp/pids/puma.pid"
+set :puma_access_log, "#{release_path}/log/puma.error.log"
+set :puma_error_log,  "#{release_path}/log/puma.access.log"
+set :ssh_options,     { forward_agent: true, user: fetch(:user), keys: %w(~/.ssh/id_rsa.pub) }
+set :puma_preload_app, true
+set :puma_worker_timeout, nil
+set :puma_init_active_record, true  # Change to false when not using ActiveRecord
+
+namespace :puma do
+  desc 'Create Directories for Puma Pids and Socket'
+  task :make_dirs do
+    on roles(:app) do
+      execute "mkdir #{shared_path}/tmp/sockets -p"
+      execute "mkdir #{shared_path}/tmp/pids -p"
+    end
+  end
+
+  before :start, :make_dirs
+end
+
+namespace :deploy do
+  desc "Make sure local git is in sync with remote."
+  task :check_revision do
+    on roles(:app) do
+      unless `git rev-parse HEAD` == `git rev-parse origin/master`
+        puts "WARNING: HEAD is not the same as origin/master"
+        puts "Run `git push` to sync changes."
+        exit
+      end
+    end
+  end
+
+  desc 'Initial Deploy'
+  task :initial do
+    on roles(:app) do
+      before 'deploy:restart', 'puma:start'
+      invoke 'deploy'
+    end
+  end
+
+  desc 'Restart application'
+  task :restart do
+    on roles(:app), in: :sequence, wait: 5 do
+      invoke 'puma:restart'
+    end
+  end
+
+  before :starting,     :check_revision
+  after  :finishing,    :compile_assets
+  after  :finishing,    :cleanup
+  after  :finishing,    :restart
+end
+
