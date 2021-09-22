ff06c5db2914247c81cbfa79a517b047009f35eb
Emma <emma.yeroshek@gmail.com>
2021-09-22 09:40:04 +0300

    Update deploy.rb

--- config/deploy.rb
+++ config/deploy.rb
@@ -33,7 +33,7 @@ append :linked_dirs, *%w(
 set :pty,             true
 set :use_sudo,        false
 set :stage,           :production
-set :deploy_via,      :remote_cache
+# set :deploy_via,      :remote_cache
 set :deploy_to,       "/home/#{fetch(:user)}/apps/#{fetch(:application)}"
 set :puma_bind,       "unix://#{shared_path}/tmp/sockets/#{fetch(:application)}-puma.sock"
 set :puma_state,      "#{shared_path}/tmp/pids/puma.state"
