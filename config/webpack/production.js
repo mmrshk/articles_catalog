40991c2a99eceab33291884c2b17a4d26ba40a95
Emma <emma.yeroshek@gmail.com>
2021-09-22 12:30:06 +0300

    Update package.json

--- config/webpack/production.js
+++ config/webpack/production.js
@@ -3,6 +3,7 @@ process.env.NODE_ENV = process.env.NODE_ENV || 'production'
 const environment = require('./environment')
 
 environment.loaders.append('sass-loader', {
+  test: /\.s[ac]ss$/i,
   use: [
     {
       loader: 'sass-loader',
